3350652ddb7f75a2062521d060f1ad18aeba1a49
Ahmed Ahmed <treasuremediaza@gmail.com>
2024-10-14 00:38:52 +0200

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,17 +10,16 @@ export default defineConfig({
   assetsInclude: ['**/*.JPG', '**/*.CR2'],
   root,
   plugins: [react()],
-  base:"/Site/",
   build: {
     outDir,
     emptyOutDir: true,
     rollupOptions: {
       input: {
         main: resolve(root, 'index.html'),
-        about: resolve(root, 'about', 'index.html'),
-        blog: resolve(root, 'blog', 'index.html'),
-        events: resolve(root, 'events', 'index.html'),
-        contact: resolve(root, 'contact', 'index.html'),
+        about: resolve(root, 'pages/about', 'index.html'),
+        blog: resolve(root, 'pages/blog', 'index.html'),
+        events: resolve(root, 'pages/events', 'index.html'),
+        contact: resolve(root, 'pages/contact', 'index.html'),
       }
     }
   }
