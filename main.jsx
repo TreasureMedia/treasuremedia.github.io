fcd9ccfbd1128c24f3f7cb6ce152e834dbf6f270
TreasureMedia <TreasureMedia@users.noreply.github.com>
2024-10-15 07:26:18 +0000

    deploy: 6446bd0c4c8891464ef1185ee835022a36e50252

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import MainApp from './Home.jsx'
+import MainApp from '/Home.jsx'
 import '/index.css'
 
 createRoot(document.getElementById('root')).render(
