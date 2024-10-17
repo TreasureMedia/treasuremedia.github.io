85b9034cd3795f77129ca7608515eb70dd59952d
Ahmed Ahmed <treasuremediaza@gmail.com>
2024-10-17 20:41:36 +0200

    Test 2

--- src/components/blog/Articles.jsx
+++ src/components/blog/Articles.jsx
@@ -3,9 +3,17 @@ import Featured from './Featured'
 import Article from './Article'
 import articles from '../../pages/blog/list/posts.json'
 import SearchBar from './SearchBar'
-import posts from '../../pages/blog/list/pictures/index'
+import img1 from "../../pages/blog/list/pictures/zelda.jpg"
+import txt1 from "../../pages/blog/list/zelda.txt"
 
 function Articles() {
+
+  const files = [
+    {
+      txt:txt1,
+      img:img1
+    }
+  ]
 
   return <>
     <div className='w-[100%] flex items-center justify-center flex-col'>
@@ -15,18 +23,16 @@ function Articles() {
 
         {/* Articles */}
         <div className='w-[98%] pb-10 grid grid-cols-4 gap-y-10 justify-items-center place-items-start'>
-        <img src={posts.img} alt="" />
+            
           {articles.map((item, index) => {
-            return <Article img={`${item.img}`}
+            return <Article img={`${files[0].img}`}
             title={`${item.title}`}
             author={item.author}
             text={item.introduction} 
             date={item.date}
-            url= {item.url}
+            url= {files[0].txt}
             className=""/>
           })}
-
-      
             
             
         </div>
