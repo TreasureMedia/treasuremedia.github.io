27b80020ed1b4bb80cd05bb4af30b02e0c6ed6d5
Ahmed Ahmed <treasuremediaza@gmail.com>
2024-10-17 20:33:17 +0200

    Test

---
+++ src/pages/blog/list/pictures/index.js
@@ -0,0 +1,10 @@
+const posts = {
+    img: "../../pages/blog/list/pictures/zelda.jpg",
+    title: "The Quiet Strength of Zelda Ann Hintsa: A Struggle Veteran’s Reflection on Activism, Ubuntu, and Healing",
+    date:"October 02, 2024",
+    author:"Reem Ahmed",
+    introduction: "Zelda Ann Hintsa’s life is a tapestry woven with threads of resistance, pain, love, and resilience. Born on June 13, 1958, in the harsh reality of apartheid South Africa...",
+    url:"../../pages/blog/list/zelda.txt"
+}
+
+export default posts
